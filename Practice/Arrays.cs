c8d35a410d341af8f409909e46e5989b000bfa72
tayp1n <100195623+tayp1n@users.noreply.github.com>
2022-04-10 20:37:25 +0300

    C#
    Arrays

---
+++ Practice/Arrays.cs
@@ -0,0 +1,156 @@
+﻿/*1. Заполнить одномерный массив случайными числами. Сжать
+массив, удалив из него все 0 и заполнить освободившиеся
+справа элементы значениями -1.
+2. Заполнить одномерные массив случайными числами.
+Преобразовать массив так, чтобы сначала шли отрицательные
+элементы, а потом положительные.
+3. В двумерном массиве, размеры которого вводятся с
+клавиатуры, поменять местами заданные столбцы.*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyProjC
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //1
+            //Random rand = new Random();
+            //int[] a = new int[10]; int[] a1 = new int[a.Length]; int ind = 0;
+
+            //for (int i = 0; i < a.Length; i++)
+            //    a[i] = rand.Next(0, 7);
+
+            //for (int i = 0; i < a.Length; i++)
+            //    if (a[i] != 0)
+            //    {
+            //        a1[ind] = a[i];
+            //        ind++;
+            //    }
+            //for (int i = ind; i < a1.Length; i++)
+            //    a1[i] = -1;
+
+            //Console.WriteLine(String.Join(" ", a1));
+            //2
+
+            //Random rand = new Random();
+            //int[] a = new int[10]; int[] a1 = new int[a.Length]; int ind = 0; int ind2 = 1;
+
+            //for (int i = 0; i < a.Length; i++)
+            //    a[i] = rand.Next(-7, 7);
+
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    if (a[i] >= 0)
+            //    {
+            //        a1[ind] = a[i];
+            //        ind++;
+            //    }
+            //    if (a[i] < 0)
+            //    {
+            //        a1[a1.Length - ind2] = a[i];
+            //        ind2++;
+            //    }
+            //}
+
+            //Console.WriteLine(String.Join(" ", a1));
+            //3
+            Random rand = new Random();
+            Console.Write("Input the columns: ");
+            int columns = Int32.Parse(Console.ReadLine());
+            Console.Write("Input the rows: ");
+            int rows = Int32.Parse(Console.ReadLine());
+
+            int[,] arr = new int[columns, rows];
+            Console.Clear();
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    arr[i, j] = rand.Next(0, 15);
+                    Console.Write("{0}\t", arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("Input 1 if you want to change the columns");
+            Console.WriteLine("Input 2 if you want to change the rows");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            //Console.Clear();
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Input the columns: ");
+                    int c = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Input the columns to change with: ");
+                    int c1 = Int32.Parse(Console.ReadLine());
+                    int tmpC = 0;
+
+                    for (int j = 0; j < rows; j++)
+                    {
+                        tmpC = arr[j, c];
+                        arr[j, c] = arr[j, c1];
+                        arr[j, c1] = tmpC;
+                    }
+
+                    for (int i = 0; i < columns; i++)
+                    {
+                        for (int j = 0; j < rows; j++)
+                        {
+                            Console.Write("{0}\t", arr[i, j]);
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Input the rows: ");
+                    int r = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Input the columns to change with: ");
+                    int r1 = Int32.Parse(Console.ReadLine());
+                    int tmpR = 0;
+
+                    for (int j = 0; j < columns; j++)
+                    {
+                        tmpC = arr[r, j];
+                        arr[r, j] = arr[r1, j];
+                        arr[r1, j] = tmpC;
+                    }
+
+                    for (int i = 0; i < columns; i++)
+                    {
+                        for (int j = 0; j < rows; j++)
+                        {
+                            Console.Write("{0}\t", arr[i, j]);
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+            }
+            Console.WriteLine("Input the columns: ");
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ Practice/Strings.cs
@@ -0,0 +1,98 @@
+﻿/*1. Создать метод, принимающий введенную пользователем
+строку, и выводящий на экран статистику по этой строке.
+Статистика должна включать общее кол-во символов, кол-во
+пробелом, кол-во цифр, кол-во символов пунктуации, кол-во
+букв и кол-во пробелов.
+2. Пользователь вводит строку и символ. В строке найти все
+вхождения этого символа и перевести его в верхний регистр, а
+также удалить часть строки, начиная с последнего вхождения
+этого символа и до конца.
+3. Удалить из строки повторяющиеся слова.
+4. Преобразовать все слова по правилу: удалить из слова все
+последующие вхождения первого символа.
+5. Преобразовать все слова по правилу: удалить в слове только
+последние вхождения каждого символа.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+   
+    class Program
+    {
+        //1
+
+        static void Statistics(string str)
+        {
+            int count = 0; int count2 = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != ' ')
+                {
+                    count++;
+                }
+                else
+                    count2++;
+            }
+            Console.WriteLine("Кол-во символов: " + count);
+            Console.WriteLine("Кол-во пробелов: " + count2);
+            Console.WriteLine("Кол-во чисел: " + str.Count(char.IsDigit));
+            Console.WriteLine("Кол-во символов пунктуации: " + str.Count(char.IsPunctuation));
+            Console.WriteLine("Кол-во символов букв: " + str.Count(char.IsLetter));
+
+        }
+        
+        static void Main(string[] args)
+        {
+            //1
+            Console.WriteLine("Введите строку: ");
+            string str = Console.ReadLine();
+            Statistics(str);
+            //2
+            //string theCutRes;
+
+            //Console.WriteLine("Введите строку и символ: ");
+
+            //string str = Console.ReadLine();
+            //string symbol = Console.ReadLine();
+
+            //var result = Regex.Replace(str, symbol, symbol.ToUpper());
+            //Console.WriteLine(result);
+
+            //symbol = symbol.ToUpper();
+
+            //theCutRes = result.Remove(result.LastIndexOf(symbol));
+            //Console.WriteLine(theCutRes);
+            //3
+            string SetenceString = "Hello word word my";
+            string[] data = SetenceString.Split(' ');
+            HashSet<string> set = new HashSet<string>();
+            for (int i = 0; i < data.Length; i++)
+            {
+                set.Add(data[i]);
+            }
+            set.ToList<String>().ForEach(x => Console.Write(x + ' '));
+            //4
+            //string SetenceString = "hello my world, how are you?";
+            //string symbol = SetenceString.Substring(0, 1);
+            //var result = Regex.Replace(SetenceString, symbol, "");
+            //result = result.Insert(0, symbol);
+            //Console.WriteLine(result);
+            //5
+            char[] charsToTrim = { '*', ' ', '?', '!', '.', '\'' };
+            string[] mass = (Console.ReadLine()).Split(charsToTrim, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(mass[mass.Length - 1]);
+
+
+
+
+
+        }
+
+    }
+}
